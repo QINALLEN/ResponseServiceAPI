1d7dfc70cd53088427bae03c01c4ff80a1d341df
se7en <710305973@qq.com>
2020-11-23 10:59:20 +0800

    1.modify

--- src/main/java/com/bank/api/configuration/BankPropertyConfig.java
+++ src/main/java/com/bank/api/configuration/BankPropertyConfig.java
@@ -1,20 +1,12 @@
 package com.bank.api.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@ComponentScan("com.bank.api.service")
 @EnableFeignClients
 public class BankPropertyConfig {
 
-    @ConditionalOnMissingBean(RestTemplate.class)
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 }
--- src/main/java/com/bank/api/service/BankService.java
+++ src/main/java/com/bank/api/service/BankService.java
@@ -16,11 +16,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @FeignClient(name="${com.hxyc.bank.config.name:monthly-statement}",contextId = "bankApi")
-public interface BankApi {
+public interface BankService {
 
     @PostMapping(value = {"/bank/service/applyAccount"})
     ApplyAccountBack applyAccount(@RequestBody ApplyAccountForm applyAccountForm);
 
+    @GetMapping(value = {"/bank/service/accountDetails/{accountNo}"})
+    List<AccountDetail> accountDetails(@PathVariable(value = "accountNo") String accountNo,
+                                       @RequestParam("flag") String flag);
 
     @GetMapping(value = {"/bank/service/accountDetails/{accountNo}"})
     List<AccountDetail> accountDetails(@PathVariable(value = "accountNo") String accountNo,
@@ -40,14 +43,29 @@ public interface BankApi {
                         @PathVariable(value = "planId") Integer planId,
                         @PathVariable(value = "supplierId") Integer supplierId,
                         @PathVariable(value = "type") Integer type,
+                        @PathVariable(value = "marginStatus") Integer marginStatus);
+
+    @PostMapping(value = {"/bank/service/refundApply/{demanderId}/{planId}/{supplierId}/{type}/{marginStatus}"})
+    Boolean refundApply(@PathVariable(value = "demanderId") String demanderId,
+                        @PathVariable(value = "planId") Integer planId,
+                        @PathVariable(value = "supplierId") Integer supplierId,
+                        @PathVariable(value = "type") Integer type,
+                        @PathVariable(value = "marginStatus") Integer marginStatus,
+                        @RequestParam(value = "amount")BigDecimal amount);
+
+    @PostMapping(value = {"/bank/service/refundApply/{demanderId}/{planId}/{supplierId}/{type}/{marginStatus}"})
+    Boolean refundApply(@PathVariable(value = "demanderId") String demanderId,
+                        @PathVariable(value = "planId") Integer planId,
+                        @PathVariable(value = "supplierId") Integer supplierId,
+                        @PathVariable(value = "type") Integer type,
                         @PathVariable(value = "marginStatus") Integer marginStatus,
                         @RequestParam(value = "amount")BigDecimal amount,
                         @RequestParam(value = "isAll")Integer isAll);
 
     @PostMapping(value = {"/bank/service/transferAccount/{demanderId}/{planId}/{supplierId}/{type}"})
     Boolean transferAccount(@PathVariable(value = "demanderId") String demanderId,
-                            @PathVariable(value = "planId") String planId,
-                            @PathVariable(value = "supplierId") String supplierId,
+                            @PathVariable(value = "planId") Integer planId,
+                            @PathVariable(value = "supplierId") Integer supplierId,
                             @PathVariable(value = "type") Integer type);
 
     @PostMapping(value = {"/bank/service/manual"})
